c4399a47240fb2b11bdba752ea9b310ccedf95cf
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-20 22:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
-
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        let firstName = document.querySelector('input[name="fname"]').value;
+        let lastName = document.querySelector('input[name="lname"]').value;
+        alert(`${firstName} ${lastName}`);
+    });
 }
